b6b28b2be8bc0f68c4aaaea8a2e3a668125b46a4
Roy <antonio_g.g@hotmail.es>
2017-09-07 20:44:05 +0200

    commit 07.09 20:43

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HumanListComponent } from './human-list/human-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HumanListComponent
   ],
   imports: [
     BrowserModule
